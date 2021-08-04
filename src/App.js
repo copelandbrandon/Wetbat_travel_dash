21d9a7acc3aaca6f871af342a4f8f13df2bd7645
Brandon <copelandbrandon2@gmail.com>
2021-08-04 14:22:14 -0600

    implement welcome widget and integrate into app

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Nav from "./components/Nav/Nav_bar";
 import Sidebar from "./components/Sidebar/Sidebar";
+import Welcome from "./components/Welcome_widget/Welcome";
 
 function App() {
   return (
     <main>
       <Nav/>
+      <div className="layout-container">
       <Sidebar/>
+      <Welcome/>
+      </div>
     </main>
   );
 }
--- src/components/Welcome_widget/Welcome.jsx
+++ src/components/Welcome_widget/Welcome.jsx
@@ -1,8 +1,30 @@
 import React from "react";
-
+import "./Welcome.css";
+import image from "./Images/clip.png";
 export default function Welcome(props) {
 
   return (
-    <div>welcome</div>
+    <div className="widget">
+      <span className="widget-left">
+        <header className="title">
+          Welcome to your dashboard
+        </header>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </span>
+      <span className="widget-right">
+        <img src={image} className="image"/>
+        <span className="welcome-values">
+          <p><b className="value-number">{props.newLeads}</b> NEW LEADS</p>
+          <p><b className="value-number">{props.createdQuotes}</b> QUOTES CREATED</p>
+          <p><b className="value-number">{props.pendingOrders}</b> PENDING ORDERS</p>
+        </span>
+      </span>
+    </div>
   );
 };
